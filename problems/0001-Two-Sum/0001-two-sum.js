2b42189a17216e0b83ba87c0b7faece2841d9c68
John Grib <johngrib82@gmail.com>
2019-01-01 19:14:09 +0900

    Fix 01 js code more readable

--- problems/0001-Two-Sum/0001-two-sum.js
+++ problems/0001-Two-Sum/0001-two-sum.js
@@ -1,27 +1,41 @@
 // https://leetcode.com/problems/two-sum/description/
-var twoSum = function(nums, target) {
-    var map = {};
-    for(var i = 0, len = nums.length; i < len; i++) {
-        var v = nums[i];
-        var n = target - v;
+const twoSum = function(nums, target) {
+    const map = {};
+
+    for (let i = 0, len = nums.length; i < len; i++) {
+        const v = nums[i];
+        const n = target - v;
+
         if (map[n] != undefined) {
             return [map[n], i];
         }
         map[v] = i;
     }
-    return []
+    return [];
 };
 
 
 const assert = require('assert');
 
-describe('twoSum', function () {
-    it('basic', function () {
-        var result = twoSum([2,7,11,15], 9);
-        assert.equal(String(result), String([0,1]));
+describe('twoSum', function() {
+    context('with a basic case', function() {
+        const given = [2, 7, 11, 15];
+        const target = 9;
+
+        it('returns indices of 2 and 7', function() {
+            const result = twoSum(given, target);
+
+            assert.equal(String(result), String([0, 1]));
+        })
     })
-    it('custom', function () {
-        var result = twoSum([3,3], 6);
-        assert.equal(String(result), String([0,1]));
+    context('with a custom case', function() {
+        const given = [3, 3];
+        const target = 6;
+
+        it('returns indices of 3 and 3', function() {
+            const result = twoSum(given, target);
+
+            assert.equal(String(result), String([0, 1]));
+        })
     })
 })
